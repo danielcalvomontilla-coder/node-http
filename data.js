6b5ef423f1a8a73412fd24e8d8a593abd93db306
Daniel <danielcalvomontilla@gmail.com>
2026-01-11 03:18:56 +0100

    final

---
+++ data.js
@@ -0,0 +1,7 @@
+const webPageInfo = {
+    title:"My Web Page",
+    subtitle:"Welcome to my web page",
+    description:"This is a simple web page created using Node.js"
+}
+
+module.exports = webPageInfo;
